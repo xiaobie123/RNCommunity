3355cf1c1c3dca4b416677bc19e9eda0deee56dc
biehengjie <biehengjie@vichain.com>
2018-07-09 19:35:26 +0800

    SectionList 组件预演

--- app/containers/my/index.js
+++ app/containers/my/index.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  SectionList,
+} from 'react-native';
 import { connect } from 'react-redux';
 
 import { Button } from '../../components';
@@ -17,7 +23,6 @@ class Account extends Component {
       />
     ),
   }
-
   gotoLogin = () => {
     this.props.dispatch(NavigationActions.navigate({ routeName: 'Login' }));
   }
@@ -27,12 +32,33 @@ class Account extends Component {
   logout = () => {
     this.props.dispatch(createAction('app/logout')());
   }
-
+  sectionComp = (info) => {
+    const txt = info.section.key;
+    return <Text
+      style={{ height: 50, textAlign: 'center', textAlignVertical: 'center', backgroundColor: '#9CEBBC', color: 'white', fontSize: 30 }}>{txt}</Text>
+  }
+  renderItem = (info) => {
+    const txt = '  ' + info.item.title;
+    return <Text style={{ height: 60, textAlignVertical: 'center', backgroundColor: '#ffffff', color: '#5C5C5C', fontSize: 15 }}>{txt}</Text>
+  }
   render() {
-    const { login } = this.props;
+    const sections = [
+      { key: "A", data: [{ title: "阿童木" }, { title: "阿玛尼" }, { title: "爱多多" }] },
+      { key: "B", data: [{ title: "表哥" }, { title: "贝贝" }, { title: "表弟" }, { title: "表姐" }, { title: "表叔" }] },
+      { key: "C", data: [{ title: "成吉思汗" }, { title: "超市快递" }] },
+      { key: "W", data: [{ title: "王磊" }, { title: "王者荣耀" }, { title: "往事不能回味" },{ title: "王小磊" }, { title: "王中磊" }, { title: "王大磊" }] },
+    ];
+
     return (
-      <View style={styles.container}>
-        <Button text="Goto Login" onPress={this.gotomyhomepage} />
+      <View style={{ flex: 1 }}>
+        <SectionList
+          renderSectionHeader={this.sectionComp}
+          renderItem={this.renderItem}
+          sections={sections}
+          ItemSeparatorComponent={() => <View><Text></Text></View>}
+          ListHeaderComponent={() => <View style={{ backgroundColor: '#25B960', alignItems: 'center', height: 30 }}><Text style={{ fontSize: 18, color: '#ffffff' }}>通讯录</Text></View>}
+          ListFooterComponent={() => <View style={{ backgroundColor: '#25B960', alignItems: 'center', height: 30 }}><Text style={{ fontSize: 18, color: '#ffffff' }}>通讯录尾部</Text></View>}
+        />
       </View>
     );
   }
